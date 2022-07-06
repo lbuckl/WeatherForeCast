57552938aa11e87365d9066dda9dc6d01b820f62
lbuckl <vadimufastr@gmail.com>
2022-07-06 21:46:58 +0300

    Закоментировал свои изыскания и обработал onCleared

--- app/src/main/java/com/gb/k_2135_2136_2/view/weatherlist/WeatherListFragment.kt
+++ app/src/main/java/com/gb/k_2135_2136_2/view/weatherlist/WeatherListFragment.kt
@@ -1,37 +1,41 @@
 package com.gb.k_2135_2136_2.view.weatherlist
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.NonNull
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.gb.k_2135_2136_2.databinding.FragmentWeatherListBinding
 import com.gb.k_2135_2136_2.viewmodel.AppState
 import com.google.android.material.snackbar.Snackbar
-import java.util.concurrent.Semaphore
 
 class WeatherListFragment : Fragment() {
-
 
     companion object {
         fun newInstance() = WeatherListFragment()
     }
-    lateinit var v: View
     lateinit var binding: FragmentWeatherListBinding
     lateinit var viewModel: WeatherListViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        //Биндинг для прямой связи View
         binding = FragmentWeatherListBinding.inflate(inflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        v = view
         super.onViewCreated(view, savedInstanceState)
+        //Получаем ссылку на наш экземпляр ViewModel
         viewModel = ViewModelProvider(this).get(WeatherListViewModel::class.java)
+        //получаем данные из LiveData и
         //запускаем подписку на AppState
         viewModel.getLiveData().observe(viewLifecycleOwner,object : Observer<AppState>{
             override fun onChanged(t: AppState) {
@@ -41,7 +45,7 @@ class WeatherListFragment : Fragment() {
         viewModel.sentRequest()
     }
 
-
+    //Подписка на изменение AppState и выполнение операций по триггеру
     private fun renderData(appState: AppState){
         when (appState){
             is AppState.Error -> {/*TODO HW*/
@@ -51,7 +55,7 @@ class WeatherListFragment : Fragment() {
             }
 
             is AppState.Loading -> {/*TODO HW*/
-                Snackbar.make(v, "Loading", Snackbar.LENGTH_LONG).show()
+                Snackbar.make(binding.root, "Loading", Snackbar.LENGTH_LONG).show()
             }
 
             is AppState.Success -> {
@@ -64,6 +68,16 @@ class WeatherListFragment : Fragment() {
                 binding.cityCoordinates.text = "${result.city.lat}/${result.city.lon}"
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("TEST", "onDestroy Fragment!!!")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("TEST", "onStop Fragment!!!")
     }
 
 }
--- app/src/main/java/com/gb/k_2135_2136_2/view/weatherlist/WeatherListViewModel.kt
+++ app/src/main/java/com/gb/k_2135_2136_2/view/weatherlist/WeatherListViewModel.kt
@@ -1,22 +1,25 @@
 package com.gb.k_2135_2136_2.view.weatherlist
 
 import android.util.Log
-import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import com.gb.k_2135_2136_2.model.Repository
 import com.gb.k_2135_2136_2.model.RepositoryLocalImpl
 import com.gb.k_2135_2136_2.model.RepositoryRemoteImpl
 import com.gb.k_2135_2136_2.viewmodel.AppState
-import java.lang.Thread.sleep
 import java.util.*
 
+/**
+ * Класс для реализации LiveData
+ * Тригерит состояния AppState
+ */
 class WeatherListViewModel(private val liveData: MutableLiveData<AppState> = MutableLiveData<AppState>()) :
     ViewModel() {
 
     lateinit var repository: Repository
 
-    //Возвращаем лайвдата
+    //Выбираем БД и возвращаем данные
     fun getLiveData():MutableLiveData<AppState>{
         choiceRepository()
         return liveData
@@ -31,24 +34,30 @@ class WeatherListViewModel(private val liveData: MutableLiveData<AppState> = Mut
         }
     }
 
+    //Непосредственно действия
     fun sentRequest() {
-        liveData.value = AppState.Loading// изменил
+        //Имитируем загрузку
+        liveData.value = AppState.Loading
+        //Рандомим результат загрузки
         val rand = Random(System.nanoTime())
         val randVal = rand.nextInt(3)
         Log.d("RandVal", "$randVal")
-        if(randVal == 1){ //FIXME
+        if(randVal == 1){
             liveData.postValue(AppState.Error(IllegalStateException("Что-то пошло не так")))
         }else{
             liveData.postValue(AppState.Success(repository.getWeather(55.755826, 37.617299900000035)))
         }
-
     }
 
+    //Имитация результата состояния сединения с БД
     private fun isConnection(): Boolean {
         return false
     }
 
+    //Отменяет подписку. Выполняется после onDestroy во фрагменте
     override fun onCleared() { // TODO HW ***
         super.onCleared()
+        Log.d("TEST", "onCleared ViewModel!!!")
     }
+
 }
