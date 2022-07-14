e049c7c53afbf66394f1dc2e76669e046883b6a9
lbuckl <vadimufastr@gmail.com>
2022-07-14 23:28:32 +0300

    Урок 5 начал: Добавил ключ апи в Билдконфиг

--- app/src/main/java/com/gb/Weather/view/weatherlist/WeatherListFragment.kt
+++ app/src/main/java/com/gb/Weather/view/weatherlist/WeatherListFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.gb.Weather.BuildConfig
 import com.gb.Weather.R
 import com.gb.Weather.databinding.FragmentWeatherListBinding
 import com.gb.Weather.domain.Weather
@@ -61,6 +62,7 @@ class WeatherListFragment : Fragment(), OnItemClick {
         binding_list.buttonWorld.setOnClickListener{
             viewModel.getWeatherListForWorld()
         }
+
     }
 
     //Подписка на изменение AppState и выполнение операций по триггеру
