530b9c47d495540785fb3b417553d997a84ab78d
lbuckl <vadimufastr@gmail.com>
2022-07-31 16:52:56 +0300

    Урок 9: Вывел имена телефонных контактов в созданный фрагмент

--- app/src/main/java/com/gb/Weather/view/menu/ContactsAdapter.kt
+++ app/src/main/java/com/gb/Weather/view/menu/ContactsAdapter.kt
@@ -1,56 +1,26 @@
 package com.gb.Weather.view.menu
 
-import android.Manifest
-import android.content.Intent
-import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
-import android.content.pm.PackageManager
-import android.net.Uri
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
-import com.gb.Weather.MyApp
 import com.gb.Weather.databinding.FragmentContactsListItemBinding
-import com.gb.Weather.domain.ContactNum
-import com.gb.Weather.shared.showSnackBarErrorMsg
+import com.gb.Weather.databinding.FragmentWeatherListItemBinding
+import com.gb.Weather.domain.Weather
 
-class ContactsAdapter(private val contactList: List<ContactNum>):
+class ContactsAdapter(private val contactList: List<String>):
     RecyclerView.Adapter<ContactsAdapter.ContactViewHolder>(){
-
-
-    lateinit var binding: FragmentContactsListItemBinding
-
 
     //Создаёт ViewHolder объект опираясь на их количество, но с запасом, чтобы можно было скролить
     //Возвращает наш объект класса ViewHolder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
-        binding = FragmentContactsListItemBinding.inflate(LayoutInflater.from(parent.context))
+        val binding = FragmentContactsListItemBinding.inflate(LayoutInflater.from(parent.context))
         return ContactViewHolder(binding.root)
     }
 
     //Связываем используемые текстовые метки с данными
     override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
         holder.bind(contactList[position])
-
-        holder.itemView.setOnClickListener{
-            //Проверяем есть ли разрешение на звонки
-            val permResult =
-                ContextCompat.checkSelfPermission(MyApp.getMyApp().baseContext, Manifest.permission.CALL_PHONE)
-
-            if (permResult == PackageManager.PERMISSION_GRANTED){
-                Log.d("@@@","Пытаюсь позвонить")
-                //Если разрешениеполучено, то создаём интент звонка по номеру
-                val callIntent = Intent(Intent.ACTION_CALL)
-                callIntent.setData(Uri.parse("tel:" + contactList[position].num)).addFlags(FLAG_ACTIVITY_NEW_TASK)
-                MyApp.getMyApp().applicationContext.startActivity(callIntent)
-            }
-            else {
-                binding.root.rootView.showSnackBarErrorMsg(
-                    "Необходимо зайти в настройки и дать приложению разрешение на вызовы")
-            }
-        }
     }
 
     //Возвращает количество элементов списка
@@ -58,12 +28,11 @@ class ContactsAdapter(private val contactList: List<ContactNum>):
         return contactList.size
     }
 
-    //Вложенный класс для отображения данных
+    //Вложенный класс для отображения данных в fragment_weather_list_item.xml
     inner class ContactViewHolder(view: View): RecyclerView.ViewHolder(view){
-        fun bind(contactname:ContactNum){
+        fun bind(contactname:String){
             val binding = FragmentContactsListItemBinding.bind(itemView)
-            binding.contactItem.text = contactname.name
-            binding.textNumber.text = contactname.num
+            binding.contactItem.text = contactname
         }
     }
 }
--- app/src/main/java/com/gb/Weather/view/menu/ContactsFragment.kt
+++ app/src/main/java/com/gb/Weather/view/menu/ContactsFragment.kt
@@ -1,35 +1,125 @@
 package com.gb.Weather.view.menu
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.ContentResolver
+import android.content.pm.PackageManager
+import android.database.Cursor
 import android.os.Bundle
-import android.view.*
+import android.provider.ContactsContract
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.gb.Weather.R
 import com.gb.Weather.databinding.FragmentContactsListBinding
-import com.gb.Weather.databinding.FragmentHistoryListBinding
 
 class ContactsFragment: Fragment() {
-    private var _binding_contacts: FragmentContactsListBinding? = null
-    private val binding_contacts: FragmentContactsListBinding
+    private var _binding: FragmentContactsListBinding? = null
+    private val binding: FragmentContactsListBinding
         get() {
-            return _binding_contacts!!
+            return _binding!!
         }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View {
-        _binding_contacts = FragmentContactsListBinding.inflate(inflater)
-        //инициализируем работу с меню (для скрытия кнопок)
-        setHasOptionsMenu(true)
-        return binding_contacts.root
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentContactsListBinding.inflate(inflater)
+        return binding.root
     }
 
-    //скрываем опцию меню "история"
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-        menu.findItem(R.id.menu_item_contacts)?.isVisible = false
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+
+    //Функция проверяет, получено ли разрешение на
+    private fun checkPermission() {
+        /**
+         * ContextCompat.checkSelfPermission
+         * Возвращает:PackageManager.PERMISSION_GRANTED,
+         * если у вас есть разрешение, или PackageManager.PERMISSION_DENIED, если нет.
+         */
+        val permResult =
+            ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_CONTACTS)
+        PackageManager.PERMISSION_GRANTED
+        if (permResult == PackageManager.PERMISSION_GRANTED) {
+            getContacts()
+            //Проверяем а не 2 ли это попытка запросить разрешение
+        } else if(shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS)){
+            AlertDialog.Builder(requireContext())
+                .setTitle("Доступ к контактам")
+                .setMessage("Для того, чтобы работать с контактами нужно иметь разрешение на доступ к ним")
+                .setPositiveButton("Логично") { _, _ ->
+                    permissionRequest(Manifest.permission.READ_CONTACTS)
+                }
+                .setNegativeButton("Бред") { dialog, _ -> dialog.dismiss() }
+                .create()
+                .show()
+        } else {
+            permissionRequest(Manifest.permission.READ_CONTACTS)
+        }
+    }
+
+    private fun permissionRequest(permission: String) {
+        requestPermissions(arrayOf(permission), REQUEST_CODE_READ_CONTACTS)
+    }
+
+    private val REQUEST_CODE_READ_CONTACTS = 999
+
+    //Функиця в которую ложатся все результаты запросов Permissions
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_READ_CONTACTS) {
+            //Проходимся по всем разрешениям и ищем разрешен ли нужный нам
+            for (pIndex in permissions.indices) {
+                if (permissions[pIndex] == Manifest.permission.READ_CONTACTS
+                    && grantResults[pIndex] == PackageManager.PERMISSION_GRANTED
+                ) {
+
+                    getContacts()
+                    Log.d("@@@", "Ура")
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    //Функция для запроса контактов
+    @SuppressLint("Range")
+    private fun getContacts() {
+        val contentResolver: ContentResolver = requireContext().contentResolver
+        // Отправляем запрос на получение контактов и получаем ответ в виде Cursor
+        val cursorWithContacts: Cursor? = contentResolver.query(
+            ContactsContract.Contacts.CONTENT_URI,
+            null,
+            null,
+            null,
+            ContactsContract.Contacts.DISPLAY_NAME + " ASC"
+        )
+        val contacts = mutableListOf<String>()
+
+        cursorWithContacts?.let { cursor->
+            for(i in 0 until cursor.count){ // аналог 0..cursorWithContacts.count-1
+                cursor.moveToPosition(i)
+                    val name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                    contacts.add(name)
+                    Log.d("@@@",name)
+            }
+        }
+        cursorWithContacts?.close()
+        binding.reciclerContacts.adapter = ContactsAdapter(contacts)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        _binding_contacts = null
+        _binding = null
     }
 }
